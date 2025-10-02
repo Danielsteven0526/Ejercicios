9e9dd34be9fdba8f9d557859d95c67b7a4adb8c8
Danielsteven0526 <danielstevem15@gmail.com>
2025-10-01 20:38:45 -0500

    Resuelto ejercicio1.cpp

--- C-Classwork-main/ejercicios/ints/ejercicio1.cpp
+++ C-Classwork-main/ejercicios/ints/ejercicio1.cpp
@@ -1,10 +1,22 @@
-// Ejercicio 1: Suma de dos enteros
 #include <iostream>
 using namespace std;
+
 int main() {
-    int a, b;
-    cout << "Ingrese dos números enteros: ";
-    cin >> a >> b;
-    cout << "Suma: " << a + b << endl;
+    int n;
+    cout << "¿Cuántos números desea sumar?: ";
+    cin >> n;
+
+    int* numeros = new int[n];
+    int suma = 0;
+
+    for(int i = 0; i < n; i++) {
+        cout << "Número " << i+1 << ": ";
+        cin >> numeros[i];
+        suma += numeros[i];
+    }
+
+    cout << "Suma: " << suma << endl;
+
+    delete[] numeros;
     return 0;
 }
